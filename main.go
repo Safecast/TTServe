2ca21d618fbffbf6341e5fcb6bb200701bdf7405
Ray Ozzie <ray@ozzie.net>
2016-06-21 19:11:43 -0400

    support for temp and humidity

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 /*
  * The Things Network to Safecast Message Publisher
  *
- * Contributors: 
+ * Contributors:
  *    Ray Ozzie
  */
 
@@ -9,7 +9,7 @@ package main
 
 import (
     "bytes"
-	"strings"
+    "strings"
     "encoding/json"
     "fmt"
     MQTT "github.com/eclipse/paho.mqtt.golang"
@@ -31,6 +31,29 @@ type dnDevice struct {
 }
 
 var dnDevices []dnDevice
+
+type SafecastData struct {
+    CapturedAt   string `json:"captured_at,omitempty"`   // 2016-02-20T14:02:25Z
+    ChannelID    string `json:"channel_id,omitempty"`    // nil
+    DeviceID     string `json:"device_id,omitempty"`     // 140
+    DeviceTypeID string `json:"devicetype_id,omitempty"` // nil
+    Height       string `json:"height,omitempty"`        // 123
+    ID           string `json:"id,omitempty"`            // 972298
+    LocationName string `json:"location_name,omitempty"` // nil
+    OriginalID   string `json:"original_id,omitempty"`   // 972298
+    SensorID     string `json:"sensor_id,omitempty"`     // nil
+    StationID    string `json:"station_id,omitempty"`    // nil
+    Unit         string `json:"unit,omitempty"`          // cpm
+    UserID       string `json:"user_id,omitempty"`       // 304
+    Value        string `json:"value,omitempty"`         // 36
+    Latitude     string `json:"latitude,omitempty"`      // 37.0105
+    Longitude    string `json:"longitude,omitempty"`     // 140.9253
+    BatVoltage   string `json:"bat_voltage,omitempty"`   // 0-N volts
+    BatSOC       string `json:"bat_soc,omitempty"`       // 0%-100%
+    WirelessSNR  string `json:"wireless_snr,omitempty"`  // -127db to +127db
+    envTemp      string `json:"env_temp,omitempty"`      // Degrees centigrade
+    envHumid     string `json:"env_humid,omitempty"`     // Percent RH
+}
 
 func main() {
 
@@ -120,7 +143,7 @@ func main() {
 }
 
 func onMessageReceived(client MQTT.Client, message MQTT.Message) {
-        upQ <- message
+    upQ <- message
 }
 
 func ProcessTelecastMessage(msg *teletype.Telecast, devEui string) {
@@ -136,15 +159,15 @@ func ProcessTelecastMessage(msg *teletype.Telecast, devEui string) {
     case "/hello":
         fallthrough
     case "/hi":
-		if (argRest == "") {
-	        sendMessage(devEui, "@ttserve: Hello.")
-		} else {
-			sendMessage(devEui, "@ttserve: "+argRest)
-		}
-	case "":
-		// Do nothing, because this is just an intentional "ping" to the server
-	default:
-		broadcastMessage(message, devEui)
+        if (argRest == "") {
+            sendMessage(devEui, "@ttserve: Hello.")
+        } else {
+            sendMessage(devEui, "@ttserve: "+argRest)
+        }
+    case "":
+        // Do nothing, because this is just an intentional "ping" to the server
+    default:
+        broadcastMessage(message, devEui)
     }
 
 }
@@ -152,43 +175,11 @@ func ProcessTelecastMessage(msg *teletype.Telecast, devEui string) {
 func ProcessSafecastMessage(msg *teletype.Telecast, defaultTime string, defaultLat float32, defaultLon float32, defaultAlt int32) {
 
     // Debug
-
     fmt.Printf("Received Safecast Message:\n")
     fmt.Printf("%s\n", msg)
 
-    // Get upload parameters
-
-    SafecastUploadURL := "http://107.161.164.163/scripts/indextest.php?api_key=%s"
-    SafecastAppKey := "z3sHhgousVDDrCVXhzMT"
-
-    UploadURL := fmt.Sprintf(SafecastUploadURL, SafecastAppKey)
-
-    // Upload it to safecast
-
-    type SafecastData struct {
-        CapturedAt   string `json:"captured_at,omitempty"`   // 2016-02-20T14:02:25Z
-        ChannelID    string `json:"channel_id,omitempty"`    // nil
-        DeviceID     string `json:"device_id,omitempty"`     // 140
-        DeviceTypeID string `json:"devicetype_id,omitempty"` // nil
-        Height       string `json:"height,omitempty"`        // 123
-        ID           string `json:"id,omitempty"`            // 972298
-        LocationName string `json:"location_name,omitempty"` // nil
-        OriginalID   string `json:"original_id,omitempty"`   // 972298
-        SensorID     string `json:"sensor_id,omitempty"`     // nil
-        StationID    string `json:"station_id,omitempty"`    // nil
-        Unit         string `json:"unit,omitempty"`          // cpm
-        UserID       string `json:"user_id,omitempty"`       // 304
-        Value        string `json:"value,omitempty"`         // 36
-        Latitude     string `json:"latitude,omitempty"`      // 37.0105
-        Longitude    string `json:"longitude,omitempty"`     // 140.9253
-        BatVoltage   string `json:"bat_voltage,omitempty"`   // 0-N volts
-        BatSOC       string `json:"bat_soc,omitempty"`       // 0%-100%
-        WirelessSNR  string `json:"wireless_snr,omitempty"`  // -127db to +127db
-    }
-
-    // We upload 3 records to the safecast service; here's the stuff in common to all
+    // Generate the fields common to all uploads to safecast
     sc := &SafecastData{}
-
     if (msg.DeviceIDString != nil) {
         sc.DeviceID = msg.GetDeviceIDString();
     } else if (msg.DeviceIDNumber != nil) {
@@ -196,25 +187,21 @@ func ProcessSafecastMessage(msg *teletype.Telecast, defaultTime string, defaultL
     } else {
         sc.DeviceID = "UNKNOWN";
     }
-
     if msg.CapturedAt != nil {
         sc.CapturedAt = msg.GetCapturedAt()
     } else {
         sc.CapturedAt = defaultTime
     }
-
     if msg.Latitude != nil {
         sc.Latitude = fmt.Sprintf("%f", msg.GetLatitude())
     } else {
         sc.Latitude = fmt.Sprintf("%f", defaultLat)
     }
-
     if msg.Longitude != nil {
         sc.Longitude = fmt.Sprintf("%f", msg.GetLongitude())
     } else {
         sc.Longitude = fmt.Sprintf("%f", defaultLon)
     }
-
     if msg.Altitude != nil {
         sc.Height = fmt.Sprintf("%d", msg.GetAltitude())
     } else {
@@ -223,103 +210,91 @@ func ProcessSafecastMessage(msg *teletype.Telecast, defaultTime string, defaultL
 
     // The first upload has everything
     sc1 := sc
-
     if (msg.Unit == nil) {
         sc1.Unit = "cpm"
     } else {
         sc1.Unit = fmt.Sprintf("%s", msg.GetUnit())
     }
-
     if (msg.Value == nil) {
         sc1.Value = "?"
     } else {
         sc1.Value = fmt.Sprintf("%d", msg.GetValue())
     }
-
     if (msg.BatteryVoltage != nil) {
         sc1.BatVoltage = fmt.Sprintf("%.4f", msg.GetBatteryVoltage())
     }
-
     if (msg.BatterySOC != nil) {
         sc1.BatSOC = fmt.Sprintf("%.2f", msg.GetBatterySOC())
     }
-
     if (msg.WirelessSNR != nil) {
         sc1.WirelessSNR = fmt.Sprintf("%.1f", msg.GetWirelessSNR())
     }
+    if (msg.EnvTemperature != nil) {
+        sc1.envTemp = fmt.Sprintf("%.2f", msg.GetEnvTemperature())
+    }
+    if (msg.EnvHumidity != nil) {
+        sc1.envHumid = fmt.Sprintf("%.2f", msg.GetEnvHumidity())
+    }
+	uploadToSafecast(sc1)
 
-    scJSON, _ := json.Marshal(sc1)
-    fmt.Printf("About to upload to %s:\n%s\n", UploadURL, scJSON)
+	// The following uploads have individual values
+	
+    if (msg.BatteryVoltage != nil) {
+        sc2 := sc
+        sc2.Unit = "bat_voltage"
+        sc2.Value = sc1.BatVoltage
+		uploadToSafecast(sc2)
+    }
+
+    if (msg.BatterySOC != nil) {
+        sc3 := sc
+        sc3.Unit = "bat_soc"
+        sc3.Value = sc1.BatSOC
+		uploadToSafecast(sc3)
+    }
+
+    if (msg.WirelessSNR != nil) {
+        sc4 := sc
+        sc4.Unit = "wireless_snr"
+        sc4.Value = sc1.WirelessSNR
+		uploadToSafecast(sc4)
+    }
+
+    if (msg.EnvTemperature != nil) {
+        sc5 := sc
+        sc5.Unit = "env_temp"
+        sc5.Value = sc1.envTemp
+		uploadToSafecast(sc5)
+    }
+
+    if (msg.EnvHumidity != nil) {
+        sc6 := sc
+        sc6.Unit = "env_humid"
+        sc6.Value = sc1.envHumid
+		uploadToSafecast(sc6)
+    }
+
+}
+
+// Upload the data structure to the Safecast service
+func uploadToSafecast(sc *SafecastData) {
+
+    SafecastUploadURL := "http://107.161.164.163/scripts/indextest.php?api_key=%s"
+    SafecastAppKey := "z3sHhgousVDDrCVXhzMT"
+    UploadURL := fmt.Sprintf(SafecastUploadURL, SafecastAppKey)
+
+    scJSON, _ := json.Marshal(sc)
+
     req, err := http.NewRequest("POST", UploadURL, bytes.NewBuffer(scJSON))
     req.Header.Set("User-Agent", "TTSERVE")
     req.Header.Set("Content-Type", "application/json")
     httpclient := &http.Client{}
+
     resp, err := httpclient.Do(req)
     if err != nil {
-        fmt.Printf("*** Error uploading to Safecast %s\n\n", err)
+        fmt.Printf("*** Error uploading %s to Safecast %s\n\n", sc.Unit, err)
     } else {
         resp.Body.Close()
-        fmt.Printf("Success!\n")
-    }
-
-    // The next upload has battery voltage
-    if (msg.BatteryVoltage != nil) {
-        // Prepare the data
-        sc2 := sc
-        sc2.Unit = "bat_voltage"
-        sc2.Value = sc1.BatVoltage
-        // Do the upload
-        scJSON, _ = json.Marshal(sc2)
-        req, err := http.NewRequest("POST", UploadURL, bytes.NewBuffer(scJSON))
-        req.Header.Set("User-Agent", "TTSERVE")
-        req.Header.Set("Content-Type", "application/json")
-        httpclient = &http.Client{}
-        resp, err = httpclient.Do(req)
-        if err != nil {
-            fmt.Printf("*** Error uploading bat_voltage to Safecast %s\n\n", err)
-        } else {
-            resp.Body.Close()
-        }
-    }
-
-    // The next upload has battery SOC
-    if (msg.BatterySOC != nil) {
-        // Prepare the data
-        sc3 := sc
-        sc3.Unit = "bat_soc"
-        sc3.Value = sc1.BatSOC
-        // Do the upload
-        scJSON, _ = json.Marshal(sc3)
-        req, err := http.NewRequest("POST", UploadURL, bytes.NewBuffer(scJSON))
-        req.Header.Set("User-Agent", "TTSERVE")
-        req.Header.Set("Content-Type", "application/json")
-        httpclient = &http.Client{}
-        resp, err = httpclient.Do(req)
-        if err != nil {
-            fmt.Printf("*** Error uploading bat_voltage to Safecast %s\n\n", err)
-        } else {
-            resp.Body.Close()
-        }
-    }
-
-    // The next upload has SNR
-    if (msg.WirelessSNR != nil) {
-        // Prepare the data
-        sc4 := sc
-        sc4.Unit = "wireless_snr"
-        sc4.Value = sc1.WirelessSNR
-        // Do the upload
-        scJSON, _ = json.Marshal(sc4)
-        req, err := http.NewRequest("POST", UploadURL, bytes.NewBuffer(scJSON))
-        req.Header.Set("User-Agent", "TTSERVE")
-        req.Header.Set("Content-Type", "application/json")
-        httpclient = &http.Client{}
-        resp, err = httpclient.Do(req)
-        if err != nil {
-            fmt.Printf("*** Error uploading SNR to Safecast %s\n\n", err)
-        } else {
-            resp.Body.Close()
-        }
     }
 
 }
@@ -355,27 +330,27 @@ func sendMessage(devEui string, message string) {
         fmt.Printf("t marshaling error: ", terr)
     }
 
-	jmsg := &DataDownAppReq {}
-	jmsg.Payload = tdata
-	jmsg.FPort = 1
-	jmsg.TTL = "1h"
-	jdata, jerr := json.Marshal(jmsg)
+    jmsg := &DataDownAppReq {}
+    jmsg.Payload = tdata
+    jmsg.FPort = 1
+    jmsg.TTL = "1h"
+    jdata, jerr := json.Marshal(jmsg)
     if jerr != nil {
         fmt.Printf("j marshaling error: ", jerr)
     }
 
-	fmt.Printf("Send %s: %s\n", getDnTopic(devEui), jdata)
-	mqttClient.Publish(getDnTopic(devEui), 0, false, jdata)
+    fmt.Printf("Send %s: %s\n", getDnTopic(devEui), jdata)
+    mqttClient.Publish(getDnTopic(devEui), 0, false, jdata)
 
 }
 
 // Broadcast a message to all known devices except the one specified by 'skip'
 func broadcastMessage(message string, skipDevEui string) {
-	if (skipDevEui == "") {
-		fmt.Printf("Broadcast '%s'\n", message)
-	} else {
-		fmt.Printf("Skipping %s, broadcast '%s'\n", skipDevEui, message)
-	}
+    if (skipDevEui == "") {
+        fmt.Printf("Broadcast '%s'\n", message)
+    } else {
+        fmt.Printf("Skipping %s, broadcast '%s'\n", skipDevEui, message)
+    }
     for _, e := range dnDevices {
         if (e.devEui != skipDevEui) {
             sendMessage(e.devEui, message)
