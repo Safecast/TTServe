fc08e2c234796e4a85244e942e32d04fb0c55db2
Ray Ozzie <ray@ozzie.net>
2016-06-29 19:43:38 -0400

    Cleaned up run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 while [ : ]; do
-    echo "Updating from GitHub."
+    echo "Updating from GitHub..."
     git pull https://ttserve:teletype123@github.com/rayozzie/teletype-ttserve
-    echo "Rebuilding."
+    echo "Rebuilding..."
     go build
-    echo "Starting."
+    echo "Starting..."
     ./teletype-ttserve
-    echo "Restarting."
+    echo "Restarting..."
     sleep 2s
 done
