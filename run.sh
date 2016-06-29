a35d1ee4cded54f6806f999d463ccc66e9cd9dd0
Ray Ozzie <ray@ozzie.net>
2016-06-29 16:22:26 -0400

    implemented server auto-restart

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 while [ : ]; do
     echo "Updating from GitHub..."
     git pull https://ttserve:teletype123@github.com/rayozzie/teletype-ttserve
-    echo "Rebuilding..."
+    echo "Rebuilding."
     go build
-    echo "Starting..."
+    echo "Starting."
     ./teletype-ttserve
-    echo "Restarting..."
+    echo "Restarting."
     sleep 2s
 done
