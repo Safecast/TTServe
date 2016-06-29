470aa3cc927ba96ab07f1f0b592f0b3fb677fe38
Ray Ozzie <ray@ozzie.net>
2016-06-29 13:31:23 -0400

    updated creds

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 while [ : ]; do
     echo "Updating from GitHub..."
-    git pull
+    git pull https://ttserve:teletype123@github.com/rayozzie/teletype-ttserve
     echo "Rebuilding..."
     go build
     echo "Starting...\n"
