2fc50354059c05cdd8d551276d4608324d4b349c
Ray Ozzie <ray@ozzie.net>
2016-06-29 19:10:02 -0400

    m

--- main.go
+++ main.go
@@ -733,12 +733,17 @@ func doDeviceSummary() {
             s = fmt.Sprintf("%s\n", s)
         }
 		id := sortedDevices[i].originalDeviceNo
+
 		s = fmt.Sprintf("%s<http://dev.safecast.org/en-US/measurements?device_id=%d|%10d>", s, id, id)
-		s = fmt.Sprintf("%s <http://dev.safecast.org/en-US/measurements?device_id=%d&unit=bat_voltage|V>", s, id)
-		s = fmt.Sprintf("%s <http://dev.safecast.org/en-US/measurements?device_id=%d&unit=bat_soc|%%>", s, id)
-		s = fmt.Sprintf("%s <http://dev.safecast.org/en-US/measurements?device_id=%d&unit=env_temp|T>", s, id)
-		s = fmt.Sprintf("%s <http://dev.safecast.org/en-US/measurements?device_id=%d&unit=env_humid|H>", s, id)
-		s = fmt.Sprintf("%s <http://dev.safecast.org/en-US/measurements?device_id=%d&unit=snr|S>", s, id)
+
+		s = fmt.Sprintf("%s (")
+		s = fmt.Sprintf("%s<http://dev.safecast.org/en-US/measurements?device_id=%d&unit=bat_voltage|V>", s, id)
+		s = fmt.Sprintf("%s<http://dev.safecast.org/en-US/measurements?device_id=%d&unit=bat_soc|%%>", s, id)
+		s = fmt.Sprintf("%s<http://dev.safecast.org/en-US/measurements?device_id=%d&unit=env_temp|T>", s, id)
+		s = fmt.Sprintf("%s<http://dev.safecast.org/en-US/measurements?device_id=%d&unit=env_humid|H>", s, id)
+		s = fmt.Sprintf("%s<http://dev.safecast.org/en-US/measurements?device_id=%d&unit=wireless_snr|S>", s, id)
+		s = fmt.Sprintf("%s)")
+
         if (sortedDevices[i].minutesAgo == 0) {
             s = fmt.Sprintf("%s last seen just now", s)
         } else {
