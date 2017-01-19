49f5ef021b94d9f0a98a57474e9c7a3fabab966c
Ray Ozzie <ray@ozzie.net>
2017-01-19 06:54:39 +0000

    m

--- main.go
+++ main.go
@@ -488,9 +488,12 @@ func errorString(err error) string {
 // Handle inbound HTTP requests to fetch log files
 func inboundWebLogHandler(rw http.ResponseWriter, req *http.Request) {
 
-	file := SafecastDirectory() + TTServerLogPath + "/" + req.RequestURI[len(TTServerURLLog):]
+	// Log it
+	filename := req.RequestURI[len(TTServerURLLog):]
+    fmt.Printf("%s WEB REQUEST for %s\n", time.Now().Format(logDateFormat), filename)
 
 	// Open the file
+	file := SafecastDirectory() + TTServerLogPath + "/" + filename
     fd, err := os.Open(file)
 	if err != nil {
 	    io.WriteString(rw, errorString(err))
