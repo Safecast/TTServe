685aa2d8bda6584dbc4e45234fb68dd31200c92e
Ray Ozzie <ray@ozzie.net>
2017-01-29 08:25:35 -0800

    m

--- run.sh
+++ run.sh
@@ -4,9 +4,6 @@
 # a separate shell script, github can updated it even while
 # we are executing this one that is perpetually in-use.
 
-# Trap Signal 2 (Ctrl+c) if we are really paranoid
-# trap '' 2
-
 # Loop forever
 while [ : ]; do
     ./run-this.sh
