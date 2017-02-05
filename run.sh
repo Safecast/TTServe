325fd0fd74cd8370397b54b928920d9bc063de1a
Ray Ozzie <ray@ozzie.net>
2017-02-05 14:24:51 -0500

    m

--- run.sh
+++ run.sh
@@ -5,7 +5,8 @@
 # we are executing this one that is perpetually in-use.
 
 # Make sure we're in the right directory, which is necessary via cron
-cd $HOME/go/src/github.com/rayozzie/teletype-ttserve
+export GOPATH=$HOME/go
+cd $GOPATH/src/github.com/rayozzie/teletype-ttserve
 
 # Loop forever
 while [ : ]; do
