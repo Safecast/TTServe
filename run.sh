77f8fe71094483ad22c8f5529eced50b2d8a5d52
Ray Ozzie <ray@ozzie.net>
2017-02-05 14:17:59 -0500

    m

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@
 # a separate shell script, github can updated it even while
 # we are executing this one that is perpetually in-use.
 
-# Make sure we're in the right directory
-cd $TT
+# Make sure we're in the right directory, which is necessary via cron
+cd $HOME/go/src/github.com/rayozzie/teletype-ttserve
 
 # Loop forever
 while [ : ]; do
