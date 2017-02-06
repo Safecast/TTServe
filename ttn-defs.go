b038939b551e79a82fc54dd76a4dede5f1eff3ae
Ray Ozzie <ray@ozzie.net>
2017-02-06 14:12:32 -0500

    m

--- ttn-defs.go
+++ ttn-defs.go
@@ -65,3 +65,35 @@ type DownlinkMessage struct {
 	PayloadRaw    []byte                 `json:"payload_raw,omitempty"`
 	PayloadFields map[string]interface{} `json:"payload_fields,omitempty"`
 }
+
+
+// MarshalText implements the encoding.TextMarshaler interface
+func (t JSONTime) MarshalText() ([]byte, error) {
+	if time.Time(t).IsZero() || time.Time(t).Unix() == 0 {
+		return []byte{}, nil
+	}
+	stamp := time.Time(t).UTC().Format(time.RFC3339Nano)
+	return []byte(stamp), nil
+}
+
+// UnmarshalText implements the encoding.TextUnmarshaler interface
+func (t *JSONTime) UnmarshalText(text []byte) error {
+	if len(text) == 0 {
+		*t = JSONTime{}
+		return nil
+	}
+	time, err := time.Parse(time.RFC3339Nano, string(text))
+	if err != nil {
+		return err
+	}
+	*t = JSONTime(time)
+	return nil
+}
+
+// BuildTime builds a new JSONTime
+func BuildTime(unixNano int64) JSONTime {
+	if unixNano == 0 {
+		return JSONTime{}
+	}
+	return JSONTime(time.Unix(0, 0).Add(time.Duration(unixNano)).UTC())
+}
