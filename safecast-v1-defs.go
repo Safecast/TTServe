cfdf4303bb443adc2d3bd84b6485d50bbaf3cb53
Ray Ozzie <ray@ozzie.net>
2017-02-15 16:47:35 -0500

    m

--- safecast-v1-defs.go
+++ safecast-v1-defs.go
@@ -38,145 +38,142 @@ type SafecastDataV1 struct {
 }
 
 func Decode(r io.Reader) (x *SafecastDataV1, err error) {
-	var i32 int32
-	var u32 uint32
-	var u64 uint64
-	var f32 float32
-	var f64 float64
+
 	x = new(SafecastDataV1)
-	if err = json.NewDecoder(r).Decode(x); err != nil {
+	err = json.NewDecoder(r).Decode(x)
+	if err != nil {
 		return
 	}
 
 	switch t := x.ChannelIDRaw.(type) {
 	case string:
-	    u64, err = strconv.ParseUint(t, 10, 32)
+	    u64, err := strconv.ParseUint(t, 10, 32)
 	    if err == nil {
-			u32 = uint32(u64)
+			u32 := uint32(u64)
 	        x.ChannelID = &u32
 	    }
 	case float64:
-		u32 = uint32(t)
+		u32 := uint32(t)
 		x.ChannelID = &u32
 	}
 
 	switch t := x.DeviceIDRaw.(type) {
 	case string:
-	    u64, err = strconv.ParseUint(t, 10, 32)
+	    u64, err := strconv.ParseUint(t, 10, 32)
 	    if err == nil {
-			u32 = uint32(u64)
+			u32 := uint32(u64)
 	        x.DeviceID = &u32
 	    }
 	case float64:
-		u32 = uint32(t)
+		u32 := uint32(t)
 		x.DeviceID = &u32
 	}
 
 	switch t := x.OriginalIDRaw.(type) {
 	case string:
-	    u64, err = strconv.ParseUint(t, 10, 32)
+	    u64, err := strconv.ParseUint(t, 10, 32)
 	    if err == nil {
-			u32 = uint32(u64)
+			u32 := uint32(u64)
 	        x.OriginalID = &u32
 	    }
 	case float64:
-		u32 = uint32(t)
+		u32 := uint32(t)
 		x.OriginalID = &u32
 	}
 
 	switch t := x.SensorIDRaw.(type) {
 	case string:
-	    u64, err = strconv.ParseUint(t, 10, 32)
+	    u64, err := strconv.ParseUint(t, 10, 32)
 	    if err == nil {
-			u32 = uint32(u64)
+			u32 := uint32(u64)
 	        x.SensorID = &u32
 	    }
 	case float64:
-		u32 = uint32(t)
+		u32 := uint32(t)
 		x.SensorID = &u32
 	}
 
 	switch t := x.StationIDRaw.(type) {
 	case string:
-	    u64, err = strconv.ParseUint(t, 10, 32)
+	    u64, err := strconv.ParseUint(t, 10, 32)
 	    if err == nil {
-			u32 = uint32(u64)
+			u32 := uint32(u64)
 	        x.StationID = &u32
 	    }
 	case float64:
-		u32 = uint32(t)
+		u32 := uint32(t)
 		x.StationID = &u32
 	}
 
 	switch t := x.UserIDRaw.(type) {
 	case string:
-	    u64, err = strconv.ParseUint(t, 10, 32)
+	    u64, err := strconv.ParseUint(t, 10, 32)
 	    if err == nil {
-			u32 = uint32(u64)
+			u32 := uint32(u64)
 	        x.UserID = &u32
 	    }
 	case float64:
-		u32 = uint32(t)
+		u32 := uint32(t)
 		x.UserID = &u32
 	}
 
 	switch t := x.IDRaw.(type) {
 	case string:
-	    u64, err = strconv.ParseUint(t, 10, 32)
+	    u64, err := strconv.ParseUint(t, 10, 32)
 	    if err == nil {
-			u32 = uint32(u64)
+			u32 := uint32(u64)
 	        x.ID = &u32
 	    }
 	case float64:
-		u32 = uint32(t)
+		u32 := uint32(t)
 		x.ID = &u32
 	}
 
 	switch t := x.HeightRaw.(type) {
 	case string:
-	    f64, err = strconv.ParseFloat(t, 32)
+	    f64, err := strconv.ParseFloat(t, 32)
 	    if err == nil {
-			i32 = int32(f64)
+			i32 := int32(f64)
 	        x.Height = &i32
 	    }
 	case float64:
-		i32 = int32(t)
+		i32 := int32(t)
 		x.Height = &i32
 	}
 
 	switch t := x.ValueRaw.(type) {
 	case string:
-	    f64, err = strconv.ParseFloat(t, 32)
+	    f64, err := strconv.ParseFloat(t, 32)
 	    if err == nil {
-			f32 = float32(f64)
+			f32 := float32(f64)
 	        x.Value = &f32
 	    }
 	case float64:
-		f32 = float32(t)
+		f32 := float32(t)
 		x.Value = &f32
 	}
 
 	switch t := x.LatitudeRaw.(type) {
 	case string:
-	    f64, err = strconv.ParseFloat(t, 32)
+	    f64, err := strconv.ParseFloat(t, 32)
 	    if err == nil {
-			f32 = float32(f64)
+			f32 := float32(f64)
 	        x.Latitude = &f32
 	    }
 	case float64:
-		f32 = float32(t)
+		f32 := float32(t)
 		x.Latitude = &f32
 	}
 
 	switch t := x.LongitudeRaw.(type) {
 	case string:
-	    f64, err = strconv.ParseFloat(t, 32)
+	    f64, err := strconv.ParseFloat(t, 32)
 	    if err == nil {
-			f32 = float32(f64)
+			f32 := float32(f64)
 	        x.Longitude = &f32
 	    }
 	case float64:
-		f32 = float32(t)
+		f32 := float32(t)
 		x.Longitude = &f32
 	}
 
