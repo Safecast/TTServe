7d59ff6596d949214c9d8290abe4b3c95f262454
Ray Ozzie <ray@ozzie.net>
2017-03-17 16:11:02 -0400

    m

--- dcheck.go
+++ dcheck.go
@@ -34,6 +34,16 @@ type MeasurementDataset struct {
     NewestUpload        time.Time
     MinUploadGapSecs    uint32
     MaxUploadGapSecs    uint32
+	GapsGt1week			uint32
+	GapsGt1day			uint32
+	GapsGt12hr			uint32
+	GapsGt6hr			uint32
+	GapsGt2hr			uint32
+	GapsGt1hr			uint32
+	GapsGt30m			uint32
+	GapsGt15m			uint32
+	GapsGt10m			uint32
+	GapsGt5m			uint32
     Measurements        uint32
     TestMeasurements    bool
     LoraTransports      uint32
@@ -100,6 +110,36 @@ func AggregateMeasurementIntoDataset(ds *MeasurementDataset, stat MeasurementSta
         if SecondsGap > ds.MaxUploadGapSecs {
             ds.MaxUploadGapSecs = SecondsGap
         }
+		if SecondsGap > 60 * 5 {
+			ds.GapsGt5m++
+		}
+		if SecondsGap > 60 * 10 {
+			ds.GapsGt10m++
+		}
+		if SecondsGap > 60 * 15 {
+			ds.GapsGt15m++
+		}
+		if SecondsGap > 60 * 30 {
+			ds.GapsGt30m++
+		}
+		if SecondsGap > 60 * 60 * 1 {
+			ds.GapsGt1hr++
+		}
+		if SecondsGap > 60 * 60 * 2 {
+			ds.GapsGt2hr++
+		}
+		if SecondsGap > 60 * 60 * 6 {
+			ds.GapsGt6hr++
+		}
+		if SecondsGap > 60 * 60 * 12 {
+			ds.GapsGt12hr++
+		}
+		if SecondsGap > 60 * 60 * 24 * 1 {
+			ds.GapsGt1day++
+		}
+		if SecondsGap > 60 * 60 * 24 * 7 {
+			ds.GapsGt1week++
+		}
     }
     ds.NewestUpload = stat.Uploaded
 
@@ -118,11 +158,24 @@ func GenerateDatasetSummary(ds MeasurementDataset) string {
 
     s += fmt.Sprintf("Period: %s\n", ds.LogRange)
     s += fmt.Sprintf("Measurements: %d\n", ds.Measurements)
-
+	if ds.Measurements == 0 {
+		return s
+	}
+	
     s += fmt.Sprintf("Oldest: %s\n", ds.OldestUpload.Format("2006-01-02 15:04 UTC"))
     s += fmt.Sprintf("Newest: %s\n", ds.NewestUpload.Format("2006-01-02 15:04 UTC"))
     s += fmt.Sprintf("Min gap: %d seconds\n", AgoMinutes(ds.MinUploadGapSecs/60))
     s += fmt.Sprintf("Max gap: %d seconds\n", AgoMinutes(ds.MaxUploadGapSecs))
+    s += fmt.Sprintf("Gaps >1w: %d%% (%d)\n", ds.GapsGt1week/ds.Measurements, ds.GapsGt1week)
+    s += fmt.Sprintf("Gaps >1d: %d%% (%d)\n", ds.GapsGt1day/ds.Measurements, ds.GapsGt1day)
+    s += fmt.Sprintf("Gaps >12hr: %d%% (%d)\n", ds.GapsGt12hr/ds.Measurements, ds.GapsGt12hr)
+    s += fmt.Sprintf("Gaps >6hr: %d%% (%d)\n", ds.GapsGt6hr/ds.Measurements, ds.GapsGt6hr)
+    s += fmt.Sprintf("Gaps >2hr: %d%% (%d)\n", ds.GapsGt2hr/ds.Measurements, ds.GapsGt2hr)
+    s += fmt.Sprintf("Gaps >1hr: %d%% (%d)\n", ds.GapsGt1hr/ds.Measurements, ds.GapsGt1hr)
+    s += fmt.Sprintf("Gaps >30m: %d%% (%d)\n", ds.GapsGt30m/ds.Measurements, ds.GapsGt30m)
+    s += fmt.Sprintf("Gaps >15m: %d%% (%d)\n", ds.GapsGt15m/ds.Measurements, ds.GapsGt15m)
+    s += fmt.Sprintf("Gaps >10m: %d%% (%d)\n", ds.GapsGt10m/ds.Measurements, ds.GapsGt10m)
+    s += fmt.Sprintf("Gaps >5m: %d%% (%d)\n", ds.GapsGt5m/ds.Measurements, ds.GapsGt5m)
 
     // Done
     return s
