ed7f0d3504cb411612c09b476368502cfa66aafd
Ray Ozzie <ray@ozzie.net>
2017-03-25 14:18:30 -0400

    m

--- tcp.go
+++ tcp.go
@@ -6,8 +6,10 @@
 package main
 
 import (
+	"io"
     "net"
     "fmt"
+	"bufio"
 )
 
 // Kick off TCP single-upload request server
@@ -29,32 +31,104 @@ func TcpInboundHandler() {
     defer ServerConn.Close()
 
     for {
-        buf := make([]byte, 8192)
 
+		// Accept the TCP connection
         conn, err := ServerConn.AcceptTCP()
         if err != nil {
             fmt.Printf("Error accepting TCP session: \n%v\n", err)
             continue
         }
 
-        n, err := conn.Read(buf)
+		// Create a reader on that connection
+		rdconn := bufio.NewReader(conn)
+
+		// Read the payload buffer format
+		payload_format_len := 1
+        payload_format := make([]byte, payload_format_len)
+        n, err := io.ReadFull(rdconn, payload_format)
         if err != nil {
-            fmt.Printf("TCP read error: \n%v\n", err)
+            fmt.Printf("tcp: can't read format: \n%v\n", err)
             conn.Close()
             continue
         }
+		if n != payload_format_len {
+            fmt.Printf("tcp: can't read format: %d/%d\n", n, payload_format_len)
+            conn.Close()
+            continue
+		}
+		if (payload_format[0] != BUFF_FORMAT_PB_ARRAY) {
+            fmt.Printf("tcp: unsupported buffer format: %d\n", payload_format[0])
+            conn.Close()
+            continue
+		}
 
-        remoteaddr := ipv4(conn.RemoteAddr().String())
+		// Read the number of array entries
+		payload_count_len := 1
+        payload_count := make([]byte, payload_count_len)
+        n, err = io.ReadFull(rdconn, payload_count)
+        if err != nil {
+            fmt.Printf("tcp: can't read count: \n%v\n", err)
+            conn.Close()
+            continue
+        }
+		if n != payload_count_len {
+            fmt.Printf("tcp: can't read count: %d/%d\n", n, payload_count_len)
+            conn.Close()
+            continue
+		}
+		if (payload_count[0] == 0) {
+            fmt.Printf("tcp: unsupported count: %d\n", payload_count[0])
+            conn.Close()
+            continue
+		}
 
+		// Read the length array
+		payload_entry_lengths_len := int(payload_count[0])
+        payload_entry_lengths := make([]byte, payload_entry_lengths_len)
+        n, err = io.ReadFull(rdconn, payload_entry_lengths)
+        if err != nil {
+            fmt.Printf("tcp: can't read entry_lengths: \n%v\n", err)
+            conn.Close()
+            continue
+        }
+		if n != int(payload_entry_lengths_len) {
+            fmt.Printf("tcp: can't read entry_lengths: %d/%d\n", n, payload_entry_lengths_len)
+            conn.Close()
+            continue
+		}
+
+		// Read the entries
+		payload_entries_len := 0
+		for i:=0; i<int(payload_entry_lengths_len); i++ {
+			payload_entries_len += int(payload_entry_lengths[i])
+		}
+        payload_entries := make([]byte, payload_entries_len)
+        n, err = io.ReadFull(rdconn, payload_entries)
+        if err != nil {
+            fmt.Printf("tcp: can't read entries: \n%v\n", err)
+            conn.Close()
+            continue
+        }
+		if n != payload_entries_len {
+            fmt.Printf("tcp: can't read entriess: %d/%d\n", n, payload_entries_len)
+            conn.Close()
+            continue
+		}
+
+		// Combine all that we've read
+		payload := append(payload_format, payload_count...)
+		payload = append(payload, payload_entry_lengths...)
+		payload = append(payload, payload_entries...)
+		
         // Initialize a new AppReq
         AppReq := IncomingAppReq{}
-        AppReq.SvTransport = "device-tcp:" + remoteaddr
+        AppReq.SvTransport = "device-tcp:" + ipv4(conn.RemoteAddr().String())
 
 		// Get the reply device ID
-        ReplyToDeviceId := getReplyDeviceIdFromPayload(buf[0:n])
+        ReplyToDeviceId := getReplyDeviceIdFromPayload(payload)
 		
         // Push it to be processed
-        go AppReqPushPayload(AppReq, buf[0:n], "device directly")
+        go AppReqPushPayload(AppReq, payload, "device directly")
         stats.Count.TCP++;
 
         // Is there a device ID to reply to?
