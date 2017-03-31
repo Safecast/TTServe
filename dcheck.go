b4b505b4ccd686d3e9604cb209596fce88095ddb
Ray Ozzie <ray@ozzie.net>
2017-03-31 08:46:43 -0400

    m

--- dcheck.go
+++ dcheck.go
@@ -336,7 +336,7 @@ func CheckMeasurement(sd SafecastData) MeasurementStat {
         }
         if sd.Bat.Current != nil {
             val := *sd.Bat.Current
-            if val < -2000.0 || val > 100 {
+            if val < -2000.0 || val > 1000 {
                 stat.BatWarning = true
             }
         }
