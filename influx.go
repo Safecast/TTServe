ddfc178ab1a3aed59012deda5018489f15b38f43
Ray Ozzie <ray@ozzie.net>
2017-04-02 10:37:30 -0400

    m

--- influx.go
+++ influx.go
@@ -712,16 +712,20 @@ func InfluxQuery(the_user string, the_query string) (success bool, result string
     }
 
     // Perform the query
-    q := influx.NewQuery("SELECT "+the_query, SafecastDb, "ns")
+	completeQuery := "SELECT " + the_query
+	fmt.Printf("%s %v", completeQuery, []byte(completeQuery))
+    q := influx.NewQuery(completeQuery, SafecastDb, "ns")
     q.Chunked = true
     q.ChunkSize = 100
     response, qerr := cl.Query(q)
     if qerr != nil {
+		fmt.Printf("err1 %v\n", qerr)
         return false, fmt.Sprintf("%v", qerr), 0
     }
 
     // Exit if an err
     if response.Error() != nil {
+		fmt.Printf("err2 %v\n", response.Error())
         return false, fmt.Sprintf("%v", response.Error()), 0
     }
 
