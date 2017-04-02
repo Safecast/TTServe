c4c30283c914628d8dbd071d9a1b4cee193d819b
Ray Ozzie <ray@ozzie.net>
2017-04-01 23:14:33 -0400

    m

--- slack.go
+++ slack.go
@@ -153,6 +153,7 @@ func inboundWebSlackHandler(rw http.ResponseWriter, req *http.Request) {
         if len(args) < 2 {
             sendToSafecastOps("Command format: SELECT <query>", SLACK_MSG_REPLY)
         } else {
+			fmt.Printf("*** Influx query: \"%s\"", messageAfterFirstWord)
 			success, result, numrows := InfluxQuery(user, messageAfterFirstWord)
 			if !success {
 	            sendToSafecastOps(fmt.Sprintf("Query error: %s", result), SLACK_MSG_REPLY)
