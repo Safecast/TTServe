460d4f24d8ff788b09a8fb9cf2a532ac576ae7cf
Ray Ozzie <ray@ozzie.net>
2017-04-05 16:37:26 -0400

    m

--- csv.go
+++ csv.go
@@ -44,7 +44,7 @@ func csvNew(filename string) (*os.File, error) {
     }
 
     // Write the header
-    fd.WriteString("service_uploaded,when_captured,device,STATS,dev_uptime,lnd_7318u,lnd_7318c,lnd_7128ec,loc_lat,loc_lon,loc_alt,bat_voltage,bat_charge,bat_current,gateway_lora_snr,env_temp,env_humid,env_press,pms_pm01_0,pms_pm02_5,pms_pm10_0,pms_c00_30,pms_c00_50,pms_c01_00,pms_c02_50,pms_c05_00,pms_c10_00,pms_csecs,opc_pm01_0,opc_pm02_5,opc_pm10_0,opc_c00_38,opc_c00_54,opc_c01_00,opc_c02_10,opc_c05_00,opc_c10_00,opc_csecs\r\n")
+    fd.WriteString("service_uploaded,when_captured,device,dev_uptime,lnd_7318u,lnd_7318c,lnd_7128ec,loc_lat,loc_lon,loc_alt,bat_voltage,bat_charge,bat_current,gateway_lora_snr,env_temp,env_humid,env_press,enc_temp,enc_humid,enc_press,pms_pm01_0,pms_pm02_5,pms_pm10_0,pms_c00_30,pms_c00_50,pms_c01_00,pms_c02_50,pms_c05_00,pms_c10_00,pms_csecs,opc_pm01_0,opc_pm02_5,opc_pm10_0,opc_c00_38,opc_c00_54,opc_c01_00,opc_c02_10,opc_c05_00,opc_c10_00,opc_csecs,STATS\r\n")
 
 	// Done
 	return fd, nil
@@ -58,90 +58,6 @@ func csvClose(fd *os.File) {
 
 // Append a measurement to the dataset
 func csvAppend(fd *os.File, sd *SafecastData) {
-
-    // Turn stats into a safe string for CSV
-    stats := "\""
-    if sd.Dev != nil {
-        if sd.Dev.UptimeMinutes != nil {
-            stats += fmt.Sprintf("Uptime:%d ", *sd.Dev.UptimeMinutes)
-        }
-        if sd.Dev.AppVersion != nil {
-            stats += fmt.Sprintf("AppVersion:%s ", *sd.Dev.AppVersion)
-        }
-        if sd.Dev.DeviceParams != nil {
-            stats += fmt.Sprintf("DevParams:%s ", *sd.Dev.DeviceParams)
-        }
-        if sd.Dev.GpsParams != nil {
-            stats += fmt.Sprintf("GpsParams:%s ", *sd.Dev.GpsParams)
-        }
-        if sd.Dev.ServiceParams != nil {
-            stats += fmt.Sprintf("ServiceParams:%s ", *sd.Dev.ServiceParams)
-        }
-        if sd.Dev.TtnParams != nil {
-            stats += fmt.Sprintf("TtnParams:%s ", *sd.Dev.TtnParams)
-        }
-        if sd.Dev.SensorParams != nil {
-            stats += fmt.Sprintf("SensorParams:%s ", *sd.Dev.SensorParams)
-        }
-        if sd.Dev.TransmittedBytes != nil {
-            stats += fmt.Sprintf("Sent:%d ", *sd.Dev.TransmittedBytes)
-        }
-        if sd.Dev.ReceivedBytes != nil {
-            stats += fmt.Sprintf("Rcvd:%d ", *sd.Dev.ReceivedBytes)
-        }
-        if sd.Dev.CommsResets != nil {
-            stats += fmt.Sprintf("CommsResets:%d ", *sd.Dev.CommsResets)
-        }
-        if sd.Dev.CommsFails != nil {
-            stats += fmt.Sprintf("CommsFails:%d ", *sd.Dev.CommsFails)
-        }
-        if sd.Dev.CommsPowerFails != nil {
-            stats += fmt.Sprintf("CommsPowerFails:%d ", *sd.Dev.CommsPowerFails)
-        }
-        if sd.Dev.DeviceRestarts != nil {
-            stats += fmt.Sprintf("Restarts:%d ", *sd.Dev.DeviceRestarts)
-        }
-        if sd.Dev.Motiondrops != nil {
-            stats += fmt.Sprintf("Motiondrops:%d ", *sd.Dev.Motiondrops)
-        }
-        if sd.Dev.Oneshots != nil {
-            stats += fmt.Sprintf("Oneshots:%d ", *sd.Dev.Oneshots)
-        }
-        if sd.Dev.OneshotSeconds != nil {
-            stats += fmt.Sprintf("OneshotSecs:%d ", *sd.Dev.OneshotSeconds)
-        }
-        if sd.Dev.Iccid != nil {
-            stats += fmt.Sprintf("Iccid:%s ", *sd.Dev.Iccid)
-        }
-        if sd.Dev.Cpsi != nil {
-            stats += fmt.Sprintf("Cpsi:%s ", *sd.Dev.Cpsi)
-        }
-        if sd.Dev.Dfu != nil {
-            stats += fmt.Sprintf("DFU:%s ", *sd.Dev.Dfu)
-        }
-        if sd.Dev.ModuleLora != nil {
-            stats += fmt.Sprintf("DFU:%s ", *sd.Dev.ModuleLora)
-        }
-        if sd.Dev.ModuleFona != nil {
-            stats += fmt.Sprintf("DFU:%s ", *sd.Dev.ModuleFona)
-        }
-        if sd.Dev.DeviceLabel != nil {
-            stats += fmt.Sprintf("Label:%s ", *sd.Dev.DeviceLabel)
-        }
-        if sd.Dev.FreeMem != nil {
-            stats += fmt.Sprintf("FreeMem:%d ", *sd.Dev.FreeMem)
-        }
-        if sd.Dev.NTPCount != nil {
-            stats += fmt.Sprintf("NTPCount:%d ", *sd.Dev.NTPCount)
-        }
-        if sd.Dev.LastFailure != nil {
-            stats += fmt.Sprintf("LastFailure:%s ", *sd.Dev.LastFailure)
-        }
-        if sd.Dev.Status != nil {
-            stats += fmt.Sprintf("Status:%s ", *sd.Dev.Status)
-        }
-    }
-    stats = stats + "\""
 
     // Write the stuff
     s := ""
@@ -168,7 +84,6 @@ func csvAppend(fd *os.File, sd *SafecastData) {
     }
 
     s = s + fmt.Sprintf(",%d", *sd.DeviceId)
-    s = s + fmt.Sprintf(",%s", stats)
     s = s + fmt.Sprintf(",%s", "")          // Value
     if sd.Lnd == nil {
         s += ",,,"
@@ -253,6 +168,25 @@ func csvAppend(fd *os.File, sd *SafecastData) {
             s += ","
         } else {
             s = s + fmt.Sprintf(",%f", *sd.Env.Press)
+        }
+    }
+    if sd.Dev == nil {
+        s += ",,,"
+    } else {
+        if sd.Dev.Temp == nil {
+            s += ","
+        } else {
+            s += fmt.Sprintf(",%f", *sd.Dev.Temp)
+        }
+        if sd.Dev.Humid == nil {
+            s += ","
+        } else {
+            s = s + fmt.Sprintf(",%f", *sd.Dev.Humid)
+        }
+        if sd.Dev.Press == nil {
+            s += ","
+        } else {
+            s = s + fmt.Sprintf(",%f", *sd.Dev.Press)
         }
     }
     if sd.Pms == nil {
@@ -363,6 +297,91 @@ func csvAppend(fd *os.File, sd *SafecastData) {
             s += fmt.Sprintf(",%d", *sd.Opc.CountSecs)
         }
     }
+
+    // Turn stats into a safe string for CSV
+    stats := "\""
+    if sd.Dev != nil {
+        if sd.Dev.UptimeMinutes != nil {
+            stats += fmt.Sprintf("Uptime:%d ", *sd.Dev.UptimeMinutes)
+        }
+        if sd.Dev.AppVersion != nil {
+            stats += fmt.Sprintf("AppVersion:%s ", *sd.Dev.AppVersion)
+        }
+        if sd.Dev.DeviceParams != nil {
+            stats += fmt.Sprintf("DevParams:%s ", *sd.Dev.DeviceParams)
+        }
+        if sd.Dev.GpsParams != nil {
+            stats += fmt.Sprintf("GpsParams:%s ", *sd.Dev.GpsParams)
+        }
+        if sd.Dev.ServiceParams != nil {
+            stats += fmt.Sprintf("ServiceParams:%s ", *sd.Dev.ServiceParams)
+        }
+        if sd.Dev.TtnParams != nil {
+            stats += fmt.Sprintf("TtnParams:%s ", *sd.Dev.TtnParams)
+        }
+        if sd.Dev.SensorParams != nil {
+            stats += fmt.Sprintf("SensorParams:%s ", *sd.Dev.SensorParams)
+        }
+        if sd.Dev.TransmittedBytes != nil {
+            stats += fmt.Sprintf("Sent:%d ", *sd.Dev.TransmittedBytes)
+        }
+        if sd.Dev.ReceivedBytes != nil {
+            stats += fmt.Sprintf("Rcvd:%d ", *sd.Dev.ReceivedBytes)
+        }
+        if sd.Dev.CommsResets != nil {
+            stats += fmt.Sprintf("CommsResets:%d ", *sd.Dev.CommsResets)
+        }
+        if sd.Dev.CommsFails != nil {
+            stats += fmt.Sprintf("CommsFails:%d ", *sd.Dev.CommsFails)
+        }
+        if sd.Dev.CommsPowerFails != nil {
+            stats += fmt.Sprintf("CommsPowerFails:%d ", *sd.Dev.CommsPowerFails)
+        }
+        if sd.Dev.DeviceRestarts != nil {
+            stats += fmt.Sprintf("Restarts:%d ", *sd.Dev.DeviceRestarts)
+        }
+        if sd.Dev.Motiondrops != nil {
+            stats += fmt.Sprintf("Motiondrops:%d ", *sd.Dev.Motiondrops)
+        }
+        if sd.Dev.Oneshots != nil {
+            stats += fmt.Sprintf("Oneshots:%d ", *sd.Dev.Oneshots)
+        }
+        if sd.Dev.OneshotSeconds != nil {
+            stats += fmt.Sprintf("OneshotSecs:%d ", *sd.Dev.OneshotSeconds)
+        }
+        if sd.Dev.Iccid != nil {
+            stats += fmt.Sprintf("Iccid:%s ", *sd.Dev.Iccid)
+        }
+        if sd.Dev.Cpsi != nil {
+            stats += fmt.Sprintf("Cpsi:%s ", *sd.Dev.Cpsi)
+        }
+        if sd.Dev.Dfu != nil {
+            stats += fmt.Sprintf("DFU:%s ", *sd.Dev.Dfu)
+        }
+        if sd.Dev.ModuleLora != nil {
+            stats += fmt.Sprintf("DFU:%s ", *sd.Dev.ModuleLora)
+        }
+        if sd.Dev.ModuleFona != nil {
+            stats += fmt.Sprintf("DFU:%s ", *sd.Dev.ModuleFona)
+        }
+        if sd.Dev.DeviceLabel != nil {
+            stats += fmt.Sprintf("Label:%s ", *sd.Dev.DeviceLabel)
+        }
+        if sd.Dev.FreeMem != nil {
+            stats += fmt.Sprintf("FreeMem:%d ", *sd.Dev.FreeMem)
+        }
+        if sd.Dev.NTPCount != nil {
+            stats += fmt.Sprintf("NTPCount:%d ", *sd.Dev.NTPCount)
+        }
+        if sd.Dev.LastFailure != nil {
+            stats += fmt.Sprintf("LastFailure:%s ", *sd.Dev.LastFailure)
+        }
+        if sd.Dev.Status != nil {
+            stats += fmt.Sprintf("Status:%s ", *sd.Dev.Status)
+        }
+    }
+    stats = stats + "\""
+    s = s + fmt.Sprintf(",%s", stats)
     s = s + "\r\n"
 
     fd.WriteString(s)
