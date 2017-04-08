e098a27b7b4c0f5c85564f0a43c5d8c580b59dee
Ray Ozzie <ray@ozzie.net>
2017-04-08 00:55:25 -0700

    m

--- command.go
+++ command.go
@@ -1,6 +1,6 @@
 // Copyright 2017 Inca Roads LLC.  All rights reserved.  
 // Use of this source code is governed by licenses granted by the
-// copyright holder including that found in the LICENSE file.
+// copyright holder including that found in the LICENSE file.  
 
 // Device monitoring
 package main
