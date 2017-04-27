67548d5d8ce50af1eb47f2412650dd21166a83f6
Ray Ozzie <ray@ozzie.net>
2017-04-27 00:42:04 -0400

    m

--- safecast.go
+++ safecast.go
@@ -666,13 +666,8 @@ func isDuplicate(checksum uint32) bool {
 // Update message ages and notify
 func sendSafecastCommsErrorsToSlack(PeriodMinutes uint32) {
     if httpTransactionErrors != 0 {
-        if httpTransactionErrors == 1 {
-            sendToSafecastOps(fmt.Sprintf("** Warning **  At %s UTC, one error uploading to %s:%s",
-                httpTransactionErrorTime, httpTransactionErrorUrl, httpTransactionErrorString), SLACK_MSG_UNSOLICITED_OPS)
-        } else {
-            sendToSafecastOps(fmt.Sprintf("** Warning **  At %s UTC, %d errors uploading in %d minutes to %s:%s",
-                httpTransactionErrorTime, httpTransactionErrors, PeriodMinutes, httpTransactionErrorUrl, httpTransactionErrorString), SLACK_MSG_UNSOLICITED_OPS)
-        }
+        sendToSafecastOps(fmt.Sprintf("** Warning **  In the %d mins after %s UTC there were errors uploading to %s:%s",
+            PeriodMinutes, httpTransactionErrorTime, httpTransactionErrorUrl, httpTransactionErrorString), SLACK_MSG_UNSOLICITED_OPS)
         httpTransactionErrors = 0
         httpTransactionErrorFirst = true
     }
