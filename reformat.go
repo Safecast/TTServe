b7faf3fb52f5c5ef889f2f96ad896262b8632c74
Ray Ozzie <ray@ozzie.net>
2017-09-07 06:52:28 -0700

    m

--- reformat.go
+++ reformat.go
@@ -125,19 +125,17 @@ func SafecastReformat(v1 *SafecastDataV1, isTestMeasurement bool) (deviceid uint
     sd.DeviceID = &v2DeviceID
 
     // Captured
-	fmt.Printf("*** Reformat Captured\n")
     if v1.CapturedAt != nil {
 
 		// Correct for badly formatted safecast-air data of the form 2017-9-7T2:3:4Z
 		t, err := time.Parse("2006-1-2T15:4:5Z", *v1.CapturedAt)
 		if err != nil {
 	        sd.CapturedAt = v1.CapturedAt
-			fmt.Printf("captured:'%s' err:%s\n", *sd.CapturedAt, err)
 		} else {
 			s := t.UTC().Format("2006-01-02T15:04:05Z")
 			sd.CapturedAt = &s
-			fmt.Printf("converted from '%s' to '%s'\n", *v1.CapturedAt, *sd.CapturedAt)
 		}
+
     }
 
     // Loc
