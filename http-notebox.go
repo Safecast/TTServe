64008488f4355ed55098b61e843093a4e64c8a4e
Ray Ozzie <ray@ozzie.net>
2017-11-21 21:00:04 -0500

    m

--- http-notebox.go
+++ http-notebox.go
@@ -10,6 +10,7 @@ import (
     "net/http"
     "fmt"
     "encoding/json"
+	"crypto/md5"
 )
 
 // Handle inbound HTTP requests from Notebox's via the Notehub reporter task
@@ -36,18 +37,92 @@ func inboundWebNoteboxHandler(rw http.ResponseWriter, req *http.Request) {
 
     }
 
-	// Parse it into an array of SafecastData structures
-	set := []SafecastData{}
-	err = json.Unmarshal(body, &set)
-	if err != nil {
-		fmt.Printf("*** %s cannot parse received this from %s: %s\n%s\n***\n", UploadedAt, remoteAddr, err, string(body))
-		return
-	}
+    // Parse it into an array of SafecastData structures
+    set := []SafecastData{}
+    err = json.Unmarshal(body, &set)
+    if err != nil {
+        fmt.Printf("*** %s cannot parse received this from %s: %s\n%s\n***\n", UploadedAt, remoteAddr, err, string(body))
+        return
+    }
 
-	// Display it
-	fmt.Printf("\n\n*** %s received this from %s\n%v\n%s\n***\n", UploadedAt, remoteAddr, set, body)
+    // Process each reading individually
+    for _, sd := range set {
+
+        err = ReformatFromNotebox(UploadedAt, &sd)
+        if err != nil {
+            fmt.Printf("*** cannot format incoming data from notebox: %sn", UploadedAt, remoteAddr, err)
+        }
+
+        // Report where we got it from, and when we got it
+        var svc Service
+        svc.UploadedAt = &UploadedAt
+        transportStr := "notebox:" + remoteAddr
+        svc.Transport = &transportStr
+        sd.Service = &svc
+
+        // If the data doesn't have anything useful in it, optimize it completely away.  This is
+        // observed to happen for Safecast Air from time to time
+        if sd.Opc == nil && sd.Pms == nil && sd.Env == nil && sd.Lnd == nil && sd.Bat == nil && sd.Dev == nil {
+            fmt.Printf("%s *** Ignoring because message contains no data\n", LogTime())
+            return
+        }
+
+        // Generate the CRC of the original device data
+        hash := HashSafecastData(sd)
+        sd.Service.HashMd5 = &hash
+
+        // Add info about the server instance that actually did the upload
+        sd.Service.Handler = &TTServeInstanceID
+
+        // Debug
+        fmt.Printf("\n%s Received payload for %d from %s\n", LogTime(), *sd.DeviceID, transportStr)
+        scJSON, _ := json.Marshal(sd)
+        fmt.Printf("%s\n", scJSON)
+
+        // Post to V2
+        Upload(sd)
+        WriteToLogs(sd)
+        stats.Count.HTTPRedirect++
+
+    }
 
     // A real request
     stats.Count.HTTP++
 
+}
+
+// ReformatFromNotebox reformats to our standard normalized data format
+func ReformatFromNotebox(uploadedAt string, sd *SafecastData) (err error) {
+
+    // Mark it as a test measurement
+    if sd.Dev == nil {
+        var dev Dev
+        sd.Dev = &dev
+    }
+	isTest := true
+    sd.Dev.Test = &isTest
+
+	// Convert from a device name to device number
+	if sd.DeviceUID == nil {
+		err = fmt.Errorf("missing device UID")
+		return;
+	}
+	hash := md5.Sum([]byte(*sd.DeviceUID))
+	var deviceID uint32 = 0
+	for i:=0; i<len(hash); i++ {
+		x := uint32(hash[i]) << (uint(i) % 4)
+		deviceID = deviceID ^ x
+	}
+
+	// Reserve the low 2^20 addresses for fixed allocation as per Rob agreement
+	// (see ttnode/src/io.c)
+    if (deviceID < 1048576) {
+        deviceID = ^deviceID;
+	}
+	sd.DeviceUID = nil
+	sd.DeviceID = &deviceID
+
+	// Done
+	return
+	
 }
