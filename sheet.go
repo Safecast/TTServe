2c4b4cbc2616a9e2395242803c4bcd93873216da
Ray Ozzie <ray@ozzie.net>
2019-07-29 07:55:35 -0400

    more repeatable error handling for sheet reload

--- sheet.go
+++ sheet.go
@@ -23,88 +23,72 @@ type sheetRow struct {
 var sheet []sheetRow
 
 // Statics
-var everRetrieved bool
 var lastRetrieved time.Time
-var failedRecently bool
-var lastError string
 
 // DeviceIDToSN converts a Safecast device ID to its manufacturing serial number
 func DeviceIDToSN(DeviceID uint32) (sn uint32, info string) {
     var fRetrieve bool
     var sheetData string
 
-    // Retrieve if never yet retrieved
-    if !everRetrieved {
-        fRetrieve = true
-    }
-
     // Cache for some time, for performance
-    if everRetrieved && (time.Now().Sub(lastRetrieved) / time.Minute) > 15 {
+    if (time.Now().Sub(lastRetrieved) / time.Minute) > 15 {
         fRetrieve = true
-        failedRecently = false
-    }
-
-    // If we've got an error, make sure we don't thrash every time we come in here
-    if fRetrieve && failedRecently {
-        return 0, ""
     }
 
     // Fetch and parse the sheet
     if fRetrieve {
+
+		// Set retrieved date regardless of error, so we don't thrash trying to reload
+        lastRetrieved = time.Now()
+
+		// Reload
         rsp, err := http.Get(sheetsSolarcastTracker)
         if err != nil {
-            lastError = fmt.Sprintf("%s", err)
-            failedRecently = true;
-			fmt.Printf("***** CANNOT http.Get %s\n", sheetsSolarcastTracker)
-			fmt.Printf("***** %s\n", lastError)
-            return 0, ""
-        }
-        defer rsp.Body.Close()
-        buf, err := ioutil.ReadAll(rsp.Body)
-        if err != nil {
-            lastError = fmt.Sprintf("%s", err)
-            failedRecently = true;
-			fmt.Printf("***** CANNOT ioutil.ReadAll %s\n", sheetsSolarcastTracker)
-			fmt.Printf("***** %s\n", lastError)
-            return 0, ""
-        }
+            fmt.Printf("***** CANNOT http.Get %s: %s\n", sheetsSolarcastTracker, err)
+        } else {
+            defer rsp.Body.Close()
+            buf, err := ioutil.ReadAll(rsp.Body)
+            if err != nil {
+                fmt.Printf("***** CANNOT ioutil.ReadAll %s: %s\n", sheetsSolarcastTracker, err)
+            } else {
 
-        // Parse the sheet.  If the col numbers change, this must be changed
-        sheetData = string(buf)
-        sheet = nil
-        splitContents := strings.Split(string(sheetData), "\n")
-        for _, c := range splitContents {
-            var row sheetRow
-            splitLine := strings.Split(c, ",")
-            for col, val := range splitLine {
-                switch col {
-                case 0: // A
-                    u64, err := strconv.ParseUint(val, 10, 32)
-                    if err == nil {
-                        row.sn = uint32(u64)
-                    }
-                case 1: // B
-                    u64, err := strconv.ParseUint(val, 10, 32)
-                    if err == nil {
-                        row.deviceid = uint32(u64)
-                    }
-                case 5: // F
-                    row.custodian = val
-                case 6: // G
-                    row.location = val
+                // Parse the sheet.  If the col numbers change, this must be changed
+                sheetData = string(buf)
+                sheet = nil
+                splitContents := strings.Split(string(sheetData), "\n")
+                for _, c := range splitContents {
+                    var row sheetRow
+                    splitLine := strings.Split(c, ",")
+                    for col, val := range splitLine {
+                        switch col {
+                        case 0: // A
+                            u64, err := strconv.ParseUint(val, 10, 32)
+                            if err == nil {
+                                row.sn = uint32(u64)
+                            }
+                        case 1: // B
+                            u64, err := strconv.ParseUint(val, 10, 32)
+                            if err == nil {
+                                row.deviceid = uint32(u64)
+                            }
+                        case 5: // F
+                            row.custodian = val
+                        case 6: // G
+                            row.location = val
 
+                        }
+                    }
+                    if row.deviceid != 0 {
+                        sheet = append(sheet, row)
+                    }
                 }
-            }
-            if row.deviceid != 0 {
-                sheet = append(sheet, row)
+
+                // Cache the data for future iterations
+                fmt.Printf("\n%s *** Refreshed %d entries from Google Sheets\n", LogTime(), len(splitContents))
+
             }
         }
 
-        // Cache the data for future iterations
-        fmt.Printf("\n%s *** Refreshed %d entries from Google Sheets\n", LogTime(), len(splitContents))
-        everRetrieved = true
-        lastRetrieved = time.Now()
-        failedRecently = false;
 
     }
 
@@ -130,18 +114,18 @@ func DeviceIDToSN(DeviceID uint32) (sn uint32, info string) {
         }
     }
 
-	// Not found
+    // Not found
     if !deviceIDFound {
 
-		// It was agreed with Rob t(see ttnode/src/io.c) that we would reserve the low 2^20 addresses
-		// for fixed allocation.  If we didn't find the device ID here and if it was in that range,
-		// use THAT as the serial number.
-		if (DeviceID < 1048576) {
-			return DeviceID, ""
-		}
+        // It was agreed with Rob t(see ttnode/src/io.c) that we would reserve the low 2^20 addresses
+        // for fixed allocation.  If we didn't find the device ID here and if it was in that range,
+        // use THAT as the serial number.
+        if (DeviceID < 1048576) {
+            return DeviceID, ""
+        }
 
-		// A new style device that was not found
-		fmt.Printf("*** Please enter info for device %d in the Tracker spreadsheet\n", DeviceID)
+        // A new style device that was not found
+        fmt.Printf("*** Please enter info for device %d in the Tracker spreadsheet\n", DeviceID)
         return 0, ""
     }
 
