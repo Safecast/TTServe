c35d65fb71801dcc5b942b1c4c2c25e1e7a2ac81
Ray Ozzie <ray@ozzie.net>
2020-11-09 10:54:47 -0500

    updated airnote schema to include humidity and air pressure; added two new device attributes 'last time the device moved' and 'device orientation'

--- go.mod
+++ go.mod
@@ -3,14 +3,12 @@ module github.com/Safecast/TTServe
 go 1.12
 
 require (
-	github.com/blues/note-go v1.1.17
+	github.com/blues/note-go v1.3.9
 	github.com/eclipse/paho.mqtt.golang v1.2.0
-	github.com/golang/protobuf v1.3.1
-	github.com/google/open-location-code/go v0.0.0-20190503133650-e9a0819b2bd2
-	github.com/lib/pq v1.1.1
+	github.com/golang/protobuf v1.4.3
+	github.com/google/open-location-code/go v0.0.0-20200929001750-55940b489b0e
+	github.com/lib/pq v1.8.0
 	github.com/safecast/ttproto v0.0.0-20180220033412-d66a002deafc
-	golang.org/x/crypto v0.0.0-20190506204251-e1dfcc566284 // indirect
-	golang.org/x/net v0.0.0-20190509222800-a4d6f7feada5 // indirect
-	golang.org/x/text v0.3.2 // indirect
-	golang.org/x/tools v0.0.0-20190509153222-73554e0f7805 // indirect
+	golang.org/x/net v0.0.0-20201031054903-ff519b6c9102 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
 )
--- http-note.go
+++ http-note.go
@@ -6,125 +6,129 @@
 package main
 
 import (
-    "fmt"
-	"time"
-	"strings"
-    "io/ioutil"
-    "net/http"
+	"encoding/json"
+	"fmt"
+	"hash/crc32"
+	"io/ioutil"
+	"net/http"
 	"net/url"
-    "encoding/json"
-    "hash/crc32"
+	"strings"
+	"time"
+
 	"github.com/blues/note-go/note"
 )
 
 // Schemas for the different file types
 type sensorTRACKER struct {
-	CPM float64			`json:"cpm,omitempty"`
-	Temperature float64	`json:"temperature,omitempty"`
-	Voltage float64		`json:"voltage,omitempty"`
+	CPM         float64 `json:"cpm,omitempty"`
+	Temperature float64 `json:"temperature,omitempty"`
+	Voltage     float64 `json:"voltage,omitempty"`
 }
 type sensorBAT struct {
-	Voltage float64		`json:"voltage,omitempty"`
+	Voltage float64 `json:"voltage,omitempty"`
 }
 type sensorINA struct {
-	Voltage float64		`json:"voltage,omitempty"`
-    Current float64		`json:"current,omitempty"`
+	Voltage float64 `json:"voltage,omitempty"`
+	Current float64 `json:"current,omitempty"`
 }
 type sensorBME struct {
-	Temperature float64	`json:"temp,omitempty"`
-    Humidity float64	`json:"humid,omitempty"`
-    Pressure float64	`json:"press,omitempty"`
+	Temperature float64 `json:"temp,omitempty"`
+	Humidity    float64 `json:"humid,omitempty"`
+	Pressure    float64 `json:"press,omitempty"`
 }
 type sensorRAD struct {
-	CPM float64			`json:"cpm,omitempty"`
-    Seconds float64		`json:"secs,omitempty"`
+	CPM     float64 `json:"cpm,omitempty"`
+	Seconds float64 `json:"secs,omitempty"`
 }
 type sensorAIR struct {
-	Pm01_0 float64		`json:"pm01_0,omitempty"`
-	Pm02_5 float64		`json:"pm02_5,omitempty"`
-	Pm10_0 float64		`json:"pm10_0,omitempty"`
-	Count00_30 uint32	`json:"c00_30,omitempty"`
-	Count00_50 uint32	`json:"c00_50,omitempty"`
-	Count01_00 uint32	`json:"c01_00,omitempty"`
-	Count02_50 uint32	`json:"c02_50,omitempty"`
-	Count05_00 uint32	`json:"c05_00,omitempty"`
-	Count10_00 uint32	`json:"c10_00,omitempty"`
-    CountSecs uint32	`json:"csecs,omitempty"`
-    Samples uint32		`json:"csamples,omitempty"`
-    Model string		`json:"sensor,omitempty"`
-	CardVoltage *float64 `json:"voltage,omitempty"`
-    CardTemp *float64 	`json:"temp,omitempty"`
-    Charging *bool		`json:"charging,omitempty"`
+	Pm01_0     float64  `json:"pm01_0,omitempty"`
+	Pm02_5     float64  `json:"pm02_5,omitempty"`
+	Pm10_0     float64  `json:"pm10_0,omitempty"`
+	Count00_30 uint32   `json:"c00_30,omitempty"`
+	Count00_50 uint32   `json:"c00_50,omitempty"`
+	Count01_00 uint32   `json:"c01_00,omitempty"`
+	Count02_50 uint32   `json:"c02_50,omitempty"`
+	Count05_00 uint32   `json:"c05_00,omitempty"`
+	Count10_00 uint32   `json:"c10_00,omitempty"`
+	CountSecs  uint32   `json:"csecs,omitempty"`
+	Samples    uint32   `json:"csamples,omitempty"`
+	Model      string   `json:"sensor,omitempty"`
+	Voltage    *float64 `json:"voltage,omitempty"`
+	Temp       *float64 `json:"temp,omitempty"`
+	Humid      *float64 `json:"humid,omitempty"`
+	Press      *float64 `json:"press,omitempty"`
+	Charging   *bool    `json:"charging,omitempty"`
+	USB        *bool    `json:"usb,omitempty"`
 }
 type sensorTRACK struct {
-	Lat float64			`json:"lat,omitempty"`
-	Lon float64			`json:"lon,omitempty"`
-	Distance float64	`json:"distance,omitempty"`
-	Seconds float64		`json:"seconds,omitempty"`
-	Velocity float64	`json:"velocity,omitempty"`
-	Bearing float64		`json:"bearing,omitempty"`
+	Lat      float64 `json:"lat,omitempty"`
+	Lon      float64 `json:"lon,omitempty"`
+	Distance float64 `json:"distance,omitempty"`
+	Seconds  float64 `json:"seconds,omitempty"`
+	Velocity float64 `json:"velocity,omitempty"`
+	Bearing  float64 `json:"bearing,omitempty"`
 }
-	
+
 // Handle inbound HTTP requests from individual data Notes, in test mode
 func inboundWebNoteHandlerTest(rw http.ResponseWriter, req *http.Request) {
 	noteHandler(rw, req, true)
 }
-	
+
 // Handle inbound HTTP requests from individual data Notes, in production mode
 func inboundWebNoteHandler(rw http.ResponseWriter, req *http.Request) {
 	noteHandler(rw, req, false)
 }
-	
+
 // Handle inbound HTTP requests from individual data Notes
 func noteHandler(rw http.ResponseWriter, req *http.Request, testMode bool) {
-    var body []byte
-    var err error
-	
-    // Count the request
-    stats.Count.HTTP++
+	var body []byte
+	var err error
 
-    // Get the remote address, and only add this to the count if it's likely from
-    // the internal HTTP load balancer.
-    remoteAddr, isReal, abusive := getRequestorIPv4(req)
+	// Count the request
+	stats.Count.HTTP++
+
+	// Get the remote address, and only add this to the count if it's likely from
+	// the internal HTTP load balancer.
+	remoteAddr, isReal, abusive := getRequestorIPv4(req)
 	if abusive {
 		return
 	}
-    if !isReal {
-        remoteAddr = "internal address"
-    }
-    transportStr := "notehub:" + remoteAddr
+	if !isReal {
+		remoteAddr = "internal address"
+	}
+	transportStr := "notehub:" + remoteAddr
 
-    // Read the body as a byte array
-    body, err = ioutil.ReadAll(req.Body)
-    if err != nil {
-        return
+	// Read the body as a byte array
+	body, err = ioutil.ReadAll(req.Body)
+	if err != nil {
+		return
 
-    }
+	}
 
 	// Exit if it's there's nothing there
 	if len(body) == 0 {
 		return
 	}
-	
+
 	// Unmarshal into a notehub Event structure, and exit if badly formatted
-    e := note.Event{}
-    err = json.Unmarshal(body, &e)
+	e := note.Event{}
+	err = json.Unmarshal(body, &e)
 	if err != nil {
 		return
 	}
 
 	// Convert to Safecast data, and exit if failure
-    sd, err := noteToSD(e, transportStr, testMode)
-    if err != nil {
-		fmt.Printf("NOTE ignored: %s\n%s\n", err, body);
+	sd, err := noteToSD(e, transportStr, testMode)
+	if err != nil {
+		fmt.Printf("NOTE ignored: %s\n%s\n", err, body)
 		return
-    }
+	}
 
 	// Display info about it
-    fmt.Printf("\n%s Received payload for %d %s from %s in %s\n", LogTime(), sd.DeviceID, *sd.DeviceUID, transportStr,
+	fmt.Printf("\n%s Received payload for %d %s from %s in %s\n", LogTime(), sd.DeviceID, *sd.DeviceUID, transportStr,
 		e.TowerLocation+" "+e.TowerCountry)
 
-	fmt.Printf("%s TRANSFORMED INTO:\n", body);
+	fmt.Printf("%s TRANSFORMED INTO:\n", body)
 	var sdJSON []byte
 	sdJSON, err = json.Marshal(sd)
 	if err == nil {
@@ -140,9 +144,9 @@ func noteHandler(rw http.ResponseWriter, req *http.Request, testMode bool) {
 // reserves the low 2^20 addresses for fixed allocation as per Rob agreement (see ttnode/src/io.c)
 func notecardDeviceUIDToSafecastDeviceID(notecardDeviceUID string) (safecastDeviceURN string, safecastDeviceID uint32) {
 	safecastDeviceURN = "note:" + notecardDeviceUID
-    safecastDeviceID = crc32.ChecksumIEEE([]byte(safecastDeviceURN))
-    if (safecastDeviceID < 1048576) {
-        safecastDeviceID = ^safecastDeviceID;
+	safecastDeviceID = crc32.ChecksumIEEE([]byte(safecastDeviceURN))
+	if safecastDeviceID < 1048576 {
+		safecastDeviceID = ^safecastDeviceID
 	}
 	return
 }
@@ -150,11 +154,22 @@ func notecardDeviceUIDToSafecastDeviceID(notecardDeviceUID string) (safecastDevi
 // ReformatFromNote reformats to our standard normalized data format
 func noteToSD(e note.Event, transport string, testMode bool) (sd SafecastData, err error) {
 
-    // Mark it as to whether or not it is a test measurement
+	// Mark it as to whether or not it is a test measurement
 	isTest := testMode
-    var dev Dev
-    sd.Dev = &dev
-    sd.Dev.Test = &isTest
+	var dev Dev
+	sd.Dev = &dev
+	if isTest {
+		sd.Dev.Test = &isTest
+	}
+
+	// Device movement
+	if e.Moved != 0 {
+		deviceMovedAt := time.Unix(e.Moved, 0).Format("2006-01-02T15:04:05Z")
+		dev.Moved = &deviceMovedAt
+	}
+	if e.Orientation != "" {
+		dev.Orientation = &e.Orientation
+	}
 
 	// Convert to safecast device ID
 	deviceURN, deviceID := notecardDeviceUIDToSafecastDeviceID(e.DeviceUID)
@@ -179,7 +194,7 @@ func noteToSD(e note.Event, transport string, testMode bool) (sd SafecastData, e
 	sd.DeviceContact = e.DeviceContact
 
 	// Optional app source, for accountability on every data point
-	if (false) {
+	if false {
 		sd.Source = e.App
 	}
 
@@ -190,21 +205,21 @@ func noteToSD(e note.Event, transport string, testMode bool) (sd SafecastData, e
 	}
 
 	// Service-related
-    var svc Service
-    sd.Service = &svc
-    svc.Transport = &transport
+	var svc Service
+	sd.Service = &svc
+	svc.Transport = &transport
 	if e.Routed != 0 {
 		uploadedAt := time.Unix(e.Routed, 0).Format("2006-01-02T15:04:05Z")
 		sd.UploadedAt = &uploadedAt
 	} else {
-	    uploadedAt := NowInUTC()
+		uploadedAt := NowInUTC()
 		svc.UploadedAt = &uploadedAt
 	}
 
 	// Where captured
 	if e.Where != "" {
-	    var loc Loc
-	    sd.Loc = &loc
+		var loc Loc
+		sd.Loc = &loc
 		var lat, lon float64
 		lat = float64(e.WhereLat)
 		lon = float64(e.WhereLon)
@@ -236,13 +251,17 @@ func noteToSD(e note.Event, transport string, testMode bool) (sd SafecastData, e
 	// Decompose the body with a per-notefile schema
 	switch e.NotefileID {
 
+	case "_session.qo":
+		err = fmt.Errorf("note: ignoring start-of-session event")
+		return
+
 	case "_air.qo":
 		s := sensorAIR{}
-	    err = json.Unmarshal(sensorJSON, &s)
+		err = json.Unmarshal(sensorJSON, &s)
 		if err != nil {
 			return
 		}
-	    var pms Pms
+		var pms Pms
 		pms.Pm01_0 = &s.Pm01_0
 		pms.Pm02_5 = &s.Pm02_5
 		pms.Pm10_0 = &s.Pm10_0
@@ -254,116 +273,119 @@ func noteToSD(e note.Event, transport string, testMode bool) (sd SafecastData, e
 		pms.Count10_00 = &s.Count10_00
 		pms.CountSecs = &s.CountSecs
 		pms.Samples = &s.Samples
-        pms.Model = &s.Model
+		pms.Model = &s.Model
 		sd.Pms = &pms
-		if s.CardVoltage != nil {
-		    var bat Bat
-			bat.Voltage = s.CardVoltage
+		if s.Voltage != nil {
+			var bat Bat
+			bat.Voltage = s.Voltage
 			bat.Charging = s.Charging
-		    sd.Bat = &bat
+			bat.Line = s.USB
+			sd.Bat = &bat
 		}
-		if s.CardTemp != nil {
-		    var env Env
-			env.Temp = s.CardTemp
-		    sd.Env = &env
+		if s.Temp != nil {
+			var env Env
+			env.Temp = s.Temp
+			env.Humid = s.Humid
+			env.Press = s.Press
+			sd.Env = &env
 		}
 
 	case "_track.qo":
-	    s := sensorTRACKER{}
-	    err = json.Unmarshal(sensorJSON, &s)
+		s := sensorTRACKER{}
+		err = json.Unmarshal(sensorJSON, &s)
 		if err != nil {
 			return
 		}
-	    var bat Bat
+		var bat Bat
 		bat.Voltage = &s.Voltage
-	    sd.Bat = &bat
-	    var env Env
+		sd.Bat = &bat
+		var env Env
 		env.Temp = &s.Temperature
-	    sd.Env = &env
+		sd.Env = &env
 		if s.CPM > 0 {
-		    var lnd Lnd
-		    lnd.U7318 = &s.CPM
+			var lnd Lnd
+			lnd.U7318 = &s.CPM
 			sd.Lnd = &lnd
 		}
-		
+
 	case "bat.qo":
-	    s := sensorBAT{}
-	    err = json.Unmarshal(sensorJSON, &s)
+		s := sensorBAT{}
+		err = json.Unmarshal(sensorJSON, &s)
 		if err != nil {
 			return
 		}
-	    var bat Bat
+		var bat Bat
 		bat.Voltage = &s.Voltage
-	    sd.Bat = &bat
+		sd.Bat = &bat
 
 	case "bat-ina219.qo":
-	    s := sensorINA{}
-	    err = json.Unmarshal(sensorJSON, &s)
+		s := sensorINA{}
+		err = json.Unmarshal(sensorJSON, &s)
 		if err != nil {
 			return
 		}
-	    var bat Bat
+		var bat Bat
 		bat.Voltage = &s.Voltage
 		bat.Current = &s.Current
-	    sd.Bat = &bat
-		
+		sd.Bat = &bat
+
 	case "air-bme280.qo":
-	    s := sensorBME{}
-	    err = json.Unmarshal(sensorJSON, &s)
+		s := sensorBME{}
+		err = json.Unmarshal(sensorJSON, &s)
 		if err != nil {
 			return
 		}
-	    var env Env
+		var env Env
 		env.Temp = &s.Temperature
 		env.Humid = &s.Humidity
 		env.Press = &s.Pressure
-	    sd.Env = &env
-		
+		sd.Env = &env
+
 	case "rad1-lnd7318u.qo":
 		fallthrough
 	case "rad0-lnd7318u.qo":
 		s := sensorRAD{}
-	    err = json.Unmarshal(sensorJSON, &s)
+		err = json.Unmarshal(sensorJSON, &s)
 		if err != nil {
 			return
 		}
-	    var lnd Lnd
-	    lnd.U7318 = &s.CPM
+		var lnd Lnd
+		lnd.U7318 = &s.CPM
 		sd.Lnd = &lnd
-		
+
 	case "rad1-lnd7318c.qo":
 		fallthrough
 	case "rad0-lnd7318c.qo":
 		s := sensorRAD{}
-	    err = json.Unmarshal(sensorJSON, &s)
+		err = json.Unmarshal(sensorJSON, &s)
 		if err != nil {
 			return
 		}
-	    var lnd Lnd
-	    lnd.C7318 = &s.CPM
+		var lnd Lnd
+		lnd.C7318 = &s.CPM
 		sd.Lnd = &lnd
 
 	case "rad1-lnd7128ec.qo":
 		fallthrough
 	case "rad0-lnd7128ec.qo":
 		s := sensorRAD{}
-	    err = json.Unmarshal(sensorJSON, &s)
+		err = json.Unmarshal(sensorJSON, &s)
 		if err != nil {
 			return
 		}
-	    var lnd Lnd
-	    lnd.EC7128 = &s.CPM
+		var lnd Lnd
+		lnd.EC7128 = &s.CPM
 		sd.Lnd = &lnd
 
 	case "aq0-pms7003.qo":
 		fallthrough
 	case "aq0-pms5003.qo":
 		s := sensorAIR{}
-	    err = json.Unmarshal(sensorJSON, &s)
+		err = json.Unmarshal(sensorJSON, &s)
 		if err != nil {
 			return
 		}
-	    var pms Pms
+		var pms Pms
 		pms.Pm01_0 = &s.Pm01_0
 		pms.Pm02_5 = &s.Pm02_5
 		pms.Pm10_0 = &s.Pm10_0
@@ -375,19 +397,19 @@ func noteToSD(e note.Event, transport string, testMode bool) (sd SafecastData, e
 		pms.Count10_00 = &s.Count10_00
 		pms.CountSecs = &s.CountSecs
 		pms.Samples = &s.Samples
-        model := strings.Split(strings.Split(e.NotefileID, "-")[1], ".")[0]
-        pms.Model = &model
+		model := strings.Split(strings.Split(e.NotefileID, "-")[1], ".")[0]
+		pms.Model = &model
 		sd.Pms = &pms
 
 	case "aq1-pms7003.qo":
 		fallthrough
 	case "aq1-pms5003.qo":
 		s := sensorAIR{}
-	    err = json.Unmarshal(sensorJSON, &s)
+		err = json.Unmarshal(sensorJSON, &s)
 		if err != nil {
 			return
 		}
-	    var pms Pms2
+		var pms Pms2
 		pms.Pm01_0 = &s.Pm01_0
 		pms.Pm02_5 = &s.Pm02_5
 		pms.Pm10_0 = &s.Pm10_0
@@ -399,17 +421,17 @@ func noteToSD(e note.Event, transport string, testMode bool) (sd SafecastData, e
 		pms.Count10_00 = &s.Count10_00
 		pms.CountSecs = &s.CountSecs
 		pms.Samples = &s.Samples
-        model := strings.Split(strings.Split(e.NotefileID, "-")[1], ".")[0]
-        pms.Model = &model
+		model := strings.Split(strings.Split(e.NotefileID, "-")[1], ".")[0]
+		pms.Model = &model
 		sd.Pms2 = &pms
 
 	case "track.qo":
-	    s := sensorTRACK{}
-	    err = json.Unmarshal(sensorJSON, &s)
+		s := sensorTRACK{}
+		err = json.Unmarshal(sensorJSON, &s)
 		if err != nil {
 			return
 		}
-	    var track Track
+		var track Track
 		track.Lat = &s.Lat
 		track.Lon = &s.Lon
 		track.Distance = &s.Distance
@@ -427,41 +449,40 @@ func noteToSD(e note.Event, transport string, testMode bool) (sd SafecastData, e
 
 	// Done
 	return
-	
+
 }
-	
+
 // Handle inbound HTTP requests from Notecards (from the device About screen)
 func inboundWebNotecardHandler(rw http.ResponseWriter, req *http.Request) {
 
 	// Decode the URL and determine the device ID
-    encodedNotecardDeviceUID := req.RequestURI[len(TTServerTopicNotecard):]
+	encodedNotecardDeviceUID := req.RequestURI[len(TTServerTopicNotecard):]
 	notecardDeviceUID, err := url.QueryUnescape(encodedNotecardDeviceUID)
 	if err != nil {
 		fmt.Printf("%s Notecard request error decoding Notecard DeviceUID '%s': %s\n", LogTime(), encodedNotecardDeviceUID, err)
 		return
 	}
-	
+
 	safecastDeviceURN, safecastDeviceID := notecardDeviceUIDToSafecastDeviceID(notecardDeviceUID)
 
 	// Figure out where to redirect themg
 	url := ""
-	if (false) {
+	if false {
 		url = fmt.Sprintf("http://%s%s%s%d.json",
 			TTServerHTTPAddress, TTServerTopicDeviceLog, time.Now().UTC().Format("2006-01-"), safecastDeviceID)
 	}
-	if (false) {
+	if false {
 		url = fmt.Sprintf("http://safecast.org/tilemap/prototype/best_devicefu.html?card_tall=1&card_show_tbl_mnew=0&card_show_tt_links=1&filter_nohis=1&highlights=%d",
 			safecastDeviceID)
 	}
-	if (true) {
+	if true {
 		url = fmt.Sprintf("https://grafana.safecast.cc/d/Med5wakZk/device-checker?orgId=1&var-device_urn=%s",
 			safecastDeviceURN)
 	}
 
-
-    fmt.Printf("%s Notecard request for %s being redirected to %s\n", LogTime(), notecardDeviceUID, url)
+	fmt.Printf("%s Notecard request for %s being redirected to %s\n", LogTime(), notecardDeviceUID, url)
 
 	// Perform the redirect
-    http.Redirect(rw, req, url, http.StatusTemporaryRedirect)
+	http.Redirect(rw, req, url, http.StatusTemporaryRedirect)
 
 }
