eb259953935b0de8a354ee5e605a3a3694616e59
Ray Ozzie <ray@ozzie.net>
2020-12-31 10:31:29 -0500

    implement offset/count on /devices

--- http-devices.go
+++ http-devices.go
@@ -11,17 +11,29 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 )
 
 // Handle inbound HTTP requests to fetch the entire list of devices
 func inboundWebDevicesHandler(rw http.ResponseWriter, req *http.Request) {
 	stats.Count.HTTP++
 
+	// Parse arguments to extract offset and count
+	_, args, err := HTTPArgs(req, TTServerTopicDevices)
+	if err != nil {
+		io.WriteString(rw, fmt.Sprintf("%s", err))
+		return
+	}
+	offset, _ := strconv.Atoi(args["offset"])
+	count, _ := strconv.Atoi(args["count"])
+	if count == 0 {
+		offset = 0
+	}
+
 	// Loop over the file system, tracking all devices
 	files, err := ioutil.ReadDir(SafecastDirectory() + TTDeviceStatusPath)
 	if err != nil {
-		fmt.Printf("/devices query error: %s", err)
-		io.WriteString(rw, fmt.Sprintf("ReadDir: %s", err))
+		io.WriteString(rw, fmt.Sprintf("%s", err))
 		return
 	}
 
@@ -33,6 +45,12 @@ func inboundWebDevicesHandler(rw http.ResponseWriter, req *http.Request) {
 
 		// Skip directories
 		if file.IsDir() {
+			continue
+		}
+
+		// Skip if we're still processing an offset
+		if offset > 0 {
+			offset--
 			continue
 		}
 
@@ -49,6 +67,14 @@ func inboundWebDevicesHandler(rw http.ResponseWriter, req *http.Request) {
 
 		// Copy only the "current values" to the output, not the historical data
 		allStatus = append(allStatus, dstatus.SafecastData)
+
+		// Stop if we're processing count
+		if count > 0 {
+			count--
+			if count == 0 {
+				break
+			}
+		}
 
 	}
 
--- http.go
+++ http.go
@@ -9,6 +9,9 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
 )
 
 // HTTPInboundHandler kicks off inbound messages coming from all sources, then serve HTTP
@@ -58,4 +61,69 @@ func HTTPInboundHandler() {
 func inboundWebRootHandler(rw http.ResponseWriter, req *http.Request) {
 	stats.Count.HTTP++
 	io.WriteString(rw, fmt.Sprintf("Hello. (%s)\n", ThisServerAddressIPv4))
+}
+
+// HTTPArgs parses the request URI and returns interesting things
+func HTTPArgs(req *http.Request, topic string) (target string, args map[string]string, err error) {
+	args = map[string]string{}
+
+	// Trim the request URI
+	target = req.RequestURI[len(topic):]
+
+	// If nothing left, there were no args
+	if len(target) == 0 {
+		return
+	}
+
+	// Make sure that the prefix is "/", else the pattern matcher is matching something we don't want
+	if strings.HasPrefix(target, "/") {
+		target = strings.TrimPrefix(target, "/")
+	}
+
+	// See if there is a query, and if so process it
+	str := strings.SplitN(target, "?", 2)
+	if len(str) == 1 {
+		return
+	}
+	target = str[0]
+	remainder := str[1]
+
+	// See if there is an anchor on the target, and special-case it
+	str2 := strings.Split(target, "#")
+	if len(str2) > 1 {
+		target = str2[0]
+		args["anchor"] = str2[1]
+	}
+
+	// Now that we know we have args, parse them
+	values, err2 := url.ParseQuery(remainder)
+	if err2 != nil {
+		err = err2
+		fmt.Printf("can't parse query: %s\n%s\n", err, str[1])
+		return
+	}
+
+	// Generate the return arg in the format we expect
+	for k, v := range values {
+		if len(v) == 1 {
+			str := v[0]
+
+			// Safely unquote the value.  This fails if there are NO quotes, so
+			// only replace the str value if no error occurs
+			s, err2 := strconv.Unquote(str)
+			if err2 == nil {
+				str = s
+			}
+
+			args[k], err = url.PathUnescape(str)
+			if err != nil {
+				fmt.Printf("can't unescape: %s\n%s\n", err, str)
+				return
+			}
+		}
+	}
+
+	// Done
+	return
+
 }
