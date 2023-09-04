15d4998d53fa557f7deeb2bd69936689160ecdfc
Ray Ozzie <ray@ozzie.net>
2023-09-04 09:03:31 -0400

    add noresvport to nfs mount as per AWS advisory

--- run-this.sh
+++ run-this.sh
@@ -8,7 +8,7 @@
 # Mount the EFS volume, assuming that we're now running under AWS
 while [ ! -d "$HOME/efs/safecast" ]; do
     pushd "$HOME"
-    sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 fs-dd1ad674.efs.us-west-2.amazonaws.com:/ efs
+    sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,noresvport,timeo=600,retrans=2 fs-dd1ad674.efs.us-west-2.amazonaws.com:/ efs
     popd
     sleep 5s
 done
